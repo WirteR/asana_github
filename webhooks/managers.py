2b9168eb23d016c2c058b3aa0712f30d3692a411
WirteR <kravetsillis16@gmail.com>
2020-10-13 15:28:00 +0300

    initial

--- webhooks/managers.py
+++ webhooks/managers.py
@@ -41,6 +41,7 @@ class AsanaCommentManager(AsanaManager):
                 "created_by": {
                     "name": self.author,
                 },
+                "html_text": f"<body>{self.body}<body>",
                 "text":self.body
             }}
         )

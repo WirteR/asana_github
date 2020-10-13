1f69e743f6ac5840347c2d834ffad21ab8f54cd8
WirteR <kravetsillis16@gmail.com>
2020-10-13 12:13:23 +0300

    add asana manager

--- webhooks/managers.py
+++ webhooks/managers.py
@@ -4,10 +4,9 @@ import asana
 class AsanaManager():
     def __init__(self, *args, **kwargs):
         self.client = asana.Client.access_token('1/1197770606849972:6ec58af88e7446f312e7b1c9e435baff')
-        print([(k,v) for k,v in kwargs.items()])
         self.resource = kwargs.get('type')
         self.author = kwargs.get('author')
-        self.body = kwargs.get('body'),
+        self.body = kwargs.get('body')
         self.github_id = kwargs.get('github_id')
         
         if self.resource == 'task':
@@ -18,7 +17,6 @@ class AsanaManager():
             self.task_id = kwargs.get('assignee').github_id
 
     def create_task(self):
-        print(self.title, self.body)
         response = self.client.tasks.create_task({
             'workspace': '1197770606849983',
             'name': str(self.title),
